d89abe2c96277a9604f70d073c7af61dd664dde8
chiiiihc <is0710kf@ed.ritsumei.ac.jp>
2024-06-27 19:54:58 +0900

    suteki_nata

--- scroll.js
+++ scroll.js
@@ -1,9 +1,29 @@
 
 const stickySections = [...document.querySelectorAll('.sticky_wrap')]
 
+
+
+
+
+
+
 window.addEventListener('scroll', (e) => {
-  for(let i = 0; i < stickySections.length; i++){
+  for (let i = 0; i < stickySections.length; i++) {
     transform(stickySections[i])
+  }
+
+  const element = document.querySelector(".speech");
+  const scrollY = window.scrollY;
+  if (3300 < scrollY) {
+
+    const a = scrollY - 3300
+    element.style.opacity = 0;
+    if (a > 0) {
+      element.style.opacity += a * 0.003
+    }
+
+  } else {
+    element.style.opacity = 0
   }
 })
 
